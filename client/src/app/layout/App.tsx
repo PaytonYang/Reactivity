c9f80387e5836f361f6b6c6d7ca3d6345f2aba41
Payton Yang <ptyang0629@gmail.com>
2026-04-26 21:33:10 +0800

    session 5

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,82 @@
+import { CssBaseline, Container, Box } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://localhost:5001/api/activities")
+      .then((response) => setActivities(response.data));
+
+    return () => {};
+  }, []);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find((a) => a.id === id));
+  };
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined);
+  };
+
+  const handleOpenForm = (id?: string) => {
+    if (id) {
+      handleSelectActivity(id);
+    } else {
+      handleCancelSelectActivity();
+    }
+
+    setEditMode(true);
+  };
+
+  const handleCloseForm = () => {
+    setEditMode(false);
+  };
+
+  const handleSubmitForm = (activity : Activity) => {
+    if (activity.id) {
+      setActivities(activities.map((a) => (a.id === activity.id ? activity : a)));
+      setSelectedActivity(activity);
+    } else {
+      activity.id = crypto.randomUUID();
+      setActivities([...activities, activity]);
+      setSelectedActivity(activity);
+    }
+
+    setEditMode(false);
+  };
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities(activities.filter((a) => a.id !== id));
+  };
+
+  return (
+    <Box sx={{ backgroundColor: "#eeeeee" }}>
+      <CssBaseline />
+      <NavBar openForm={handleOpenForm} />
+      <Container maxWidth="xl" sx={{ mt: 3 }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          selectedActivity={selectedActivity}
+          openForm={handleOpenForm}
+          closeForm={handleCloseForm}
+          editMode={editMode}
+          submitForm={handleSubmitForm}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </Box>
+  );
+}
+
+export default App;
---
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+
+type Props={
+    selectedActivity: Activity | undefined
+    closeForm: () => void
+    submitForm: (activity: Activity) => void
+}
+
+export default function ActivityForm({ selectedActivity, closeForm, submitForm }: Props) {
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+
+        const activity: Activity = {
+            id: selectedActivity?.id || "",
+            title: formData.get("title") as string,
+            description: formData.get("description") as string,
+            category: formData.get("category") as string,
+            date: formData.get("date") as string,
+            city: formData.get("city") as string,
+            venue: formData.get("venue") as string,
+            isCancelled: false,
+            latitude: 0,
+            longitude: 0
+        }
+
+        submitForm(activity);
+    };
+
+  return (
+    <Paper sx={{borderRadius: 3, padding: 3}}>
+        <Typography variant="h5" gutterBottom color="primary">
+            Create Activity
+        </Typography>
+        <Box component="form" display="flex" flexDirection="column" gap={3} onSubmit={handleSubmit}>
+            <TextField name="title" label="Title" defaultValue={selectedActivity?.title} />
+            <TextField name="description" label="Description" multiline rows={3} defaultValue={selectedActivity?.description} />
+            <TextField name="category" label="Category" defaultValue={selectedActivity?.category} />
+            <TextField name="date" label="Date" type="date" defaultValue={selectedActivity?.date} />
+            <TextField name="city" label="City" defaultValue={selectedActivity?.city} />
+            <TextField name="venue" label="Venue" defaultValue={selectedActivity?.venue} />
+            <Box display="flex" gap={3} justifyContent="flex-end">
+                <Button color="inherit" onClick={closeForm}>Cancel</Button>
+                <Button type="submit" variant="contained" color="success">
+                    Submit
+                </Button>
+            </Box>
+        </Box>
+    </Paper>
+  )
+}
