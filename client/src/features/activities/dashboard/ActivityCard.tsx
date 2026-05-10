3128bb2bfb4f9a924fc07078e2663f689a8d5c78
Payton Yang <ptyang0629@gmail.com>
2026-05-10 15:18:52 +0800

    session 8

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,43 +1,72 @@
-import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from '@mui/material';
-import { useActivities } from '../../../lib/hooks/useActivities';
+import AccessTime from '@mui/icons-material/AccessTime';
+import Place from '@mui/icons-material/Place';
+import { Avatar, Box, Button, Card, CardContent, CardHeader, Chip, Divider, Typography } from '@mui/material';
 import { Link } from 'react-router';
+import { formatDate } from '../../../lib/util/util';
 
 type Props = {
   activity: Activity;
 };
 
 export default function ActivityCard({ activity }: Props) {
-  const { deleteActivity } = useActivities();
+  const isHost = false; // Placeholder for host check logic
+  const isGoing = false; // Placeholder for going check logic
+  const label = isHost ? 'You are hosting' : 'You are going';
+  const isCancelled = false; // Placeholder for cancellation check logic
+  const color = isHost ? 'secondary' : isGoing ? 'warning' : 'default';
 
   return (
     <Card sx={{ borderRadius: 3 }}>
-      <CardContent>
-        <Typography variant="h5" fontWeight="bold">
-          {activity.title}
-        </Typography>
-        <Typography sx={{ color: 'text.secondary', mb: 1 }}>{activity.date}</Typography>
-        <Typography variant="body2">{activity.description}</Typography>
-        <Typography variant="subtitle1">
-          {activity.city} / {activity.venue}
-        </Typography>
-      </CardContent>
-      <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Chip label={activity.category} variant="outlined" />
-        <Box display="flex" gap={3}>
-          <Button size="medium" variant="contained" component={Link} to={`/activities/${activity.id}`}>
-            View
-          </Button>
-          <Button
-            size="medium"
-            variant="contained"
-            color="error"
-            onClick={async () => await deleteActivity.mutateAsync(activity.id)}
-            disabled={deleteActivity.isPending}
-          >
-            Delete
-          </Button>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <CardHeader
+          avatar={<Avatar sx={{ height: 80, width: 80 }} />}
+          title={activity.title}
+          slotProps={{ title: { fontWeight: 'bold', fontSize: 20 } }}
+          subheader={
+            <>
+              Host by <Link to={`/profiles/Bob`}>Bob</Link>
+            </>
+          }
+        />
+        <Box display="flex" flexDirection="column" gap={2} mr={2}>
+          {(isHost || isGoing) && <Chip label={label} color={color} sx={{ borderRadius: 2 }} />}
+          {isCancelled && <Chip label="Cancelled" color="error" sx={{ borderRadius: 2 }} />}
         </Box>
-      </CardActions>
+      </Box>
+
+      <Divider sx={{ mb: 3 }} />
+
+      <CardContent sx={{ p: 0 }}>
+        <Box display="flex" alignItems="center" mb={2} px={2}>
+          <Box display="flex" alignItems="center" flexGrow={0}>
+            <AccessTime sx={{ mr: 1 }} />
+            <Typography variant="body2" noWrap>
+              {formatDate(activity.date)}
+            </Typography>
+          </Box>
+          <Place sx={{ ml: 3, mr: 1 }} />
+          <Typography variant="body2">
+            {activity.city} / {activity.venue}
+          </Typography>
+        </Box>
+        <Divider />
+        <Box display="flex" gap={2} sx={{ backgroundColor: 'grey.200', py: 3, pl: 3 }}>
+          Addendds go here
+        </Box>
+      </CardContent>
+
+      <CardContent sx={{ pb: 2 }}>
+        <Typography variant="body2">{activity.description}</Typography>
+        <Button
+          size="medium"
+          variant="contained"
+          component={Link}
+          to={`/activities/${activity.id}`}
+          sx={{ display: 'flex', justifySelf: 'flex-end' }}
+        >
+          View
+        </Button>
+      </CardContent>
     </Card>
   );
 }
---
+++ client/src/features/activities/details/ActivityDetailsPage.tsx
@@ -0,0 +1,33 @@
+import { Grid, Typography } from '@mui/material';
+import { useParams } from 'react-router';
+import { useActivities } from '../../../lib/hooks/useActivities';
+import ActivityDetailsHeader from './ActivityDetailsHeader';
+import ActivityDetailsInfo from './ActivityDetailsInfo';
+import ActivityDetailsSidebar from './ActivityDetailsSidebar';
+import ActivityDetailsChat from './ActivityDetailsChat';
+
+export default function ActivityDetailsPage() {
+  const { id } = useParams();
+  const { activity, isLoadingActivity } = useActivities(id);
+
+  if (isLoadingActivity) {
+    return <Typography variant="h5">Loading...</Typography>;
+  }
+
+  if (!activity) {
+    return <Typography variant="h5">Activity not found</Typography>;
+  }
+
+  return (
+    <Grid container spacing={3}>
+      <Grid size={8}>
+        <ActivityDetailsHeader activity={activity} />
+        <ActivityDetailsInfo activity={activity} />
+        <ActivityDetailsChat/>
+      </Grid>
+      <Grid size={4}>
+        <ActivityDetailsSidebar/>
+      </Grid>
+    </Grid>
+  );
+}
